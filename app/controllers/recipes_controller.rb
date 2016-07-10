6ba9ea1d5368a778627cdd73c4a436fd2c1570c0
grahk93 <grace.juster@flatironschool.com>
2016-07-10 16:41:51 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,16 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
   end
 
   def create
+     @recipie=Recipe.create(recipe_params)
+     redirect_to recipe_path(@recipie)
+  end
+
+private
+  def recipe_parmas
+     params.require(:recipie).permit(:title, :ingredients_attributes [:name, :quantity])
   end
 end
